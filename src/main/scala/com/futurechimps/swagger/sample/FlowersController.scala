b0134f179e3db9de1999664fc037d9f7123a13f6
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-10-14 22:10:15 +0100

    Parameters going back in.

--- src/main/scala/com/futurechimps/swagger/sample/FlowersController.scala
+++ src/main/scala/com/futurechimps/swagger/sample/FlowersController.scala
@@ -51,7 +51,7 @@ class FlowersController(implicit val swagger: Swagger) extends ScalatraServlet
     summary("Show all flowers"),
     nickname("getFlowers"),
     responseClass("List[Flower]"),
-    //parameters(Parameter("name", "A name to search for", DataType.String, paramType = ParamType.Query, required = false)),
+    parameters(Parameter("name", "A name to search for", DataType.String, paramType = ParamType.Query, required = false)),
     endpoint(""),
     notes("Shows all the flowers in the flower shop. You can search it too.")){
     params.get("name") match {
